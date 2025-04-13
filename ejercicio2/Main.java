9a885efa90ab61386b6086cb0ff875cd66a6fffb
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:34:01 -0500

    v3

--- ejercicio2/Main.java
+++ ejercicio2/Main.java
@@ -1,5 +1,12 @@
 package ejercicio2;
 import java.util.Arrays;
 public class Main {
+    public static void main(String[] args) {
+        int[][] arreglos = {
+            {4,2,7,10,4,17},
+            {4,2,7,10,4,1,6},
+            {9,2,7,1,7}
+        };
+    }
 
 }
