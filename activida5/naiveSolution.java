3c630ed944a5373c2d8a2e49cb7aadfb1a2a0a3f
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:02:11 -0500

    v4

--- activida5/naiveSolution.java
+++ activida5/naiveSolution.java
@@ -11,3 +11,8 @@ public class naiveSolution {
         return tmpMax;
     }
 }
+public static void main(String[] args) {
+    int[] values = new int[]{3, 7, 1, 3, 9};
+    int rodLength = values.length;
+    System.out.println("el valor maximo es:" + getValue(values, rodLength));
+}
