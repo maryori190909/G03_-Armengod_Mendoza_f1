c9389cdad2b294168c038e3cbfd57eb0193472a6
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:43:25 -0500

    v3

--- ejercicio3/ViajeMasBarato.java
+++ ejercicio3/ViajeMasBarato.java
@@ -4,6 +4,13 @@ public class ViajeMasBarato {
     public static int[][] calcularCostosMinimos(int[][] T) {
         int n = T.length;
         int[][] C = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+    
+            }
+        }
     }
+    
 
 }
