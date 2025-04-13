fe7d59c11f2112bb336353c762978cd066719e1c
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:32:18 -0500

    v3

--- actividad4/ModaCalculador.java
+++ actividad4/ModaCalculador.java
@@ -1,10 +1,16 @@
 package actividad4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModaCalculador {
     public static int calcularModa(int[] arreglo) {
         if (arreglo.length == 0) {
             throw new IllegalArgumentException("el arreglo no debe estar vacio.");
         }
+        Map<Integer, Integer> frecuencias = new HashMap<>();
+        int moda = arreglo[0];
+        int maxFrecuencia = 1;
     }
 }
 
