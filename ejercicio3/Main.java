4834565b1819137c3565ed4c94bf8e656061f623
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:56:38 -0500

    v5

--- ejercicio3/Main.java
+++ ejercicio3/Main.java
@@ -3,6 +3,13 @@ package ejercicio3;
 public class Main {
     public static void main(String[] args) {
         final int INF = Integer.MAX_VALUE;
+        int[][] T = {
+            { 0,3,1,6,INF },
+            { 0,0,1,2,INF },
+            { 0,0,0,1,4 },
+            { 0,0,0,0,2 },
+            { 0,0,0,0,0 }
+        };
     }
 
 }
