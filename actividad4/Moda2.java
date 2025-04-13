5bb73af38a83f11d55d6ce8a066ed2e85ae8749e
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:45:32 -0500

    v3

--- actividad4/Moda2.java
+++ actividad4/Moda2.java
@@ -4,5 +4,17 @@ public class Moda2 {
     if (array.length == 0) {
         throw new IllegalArgumentException("El arreglo no puede estar vacío.");
     }
+    int i = 1;
+    int inicio = 0;
+    int fin = array.length - 1;
+    int moda = array[0];
+    int frecuencia = 1;
+    int maxFrecuencia = 1;
+    while (i <= fin) {
+        if (array[i] == array[inicio]) {
+            frecuencia++;
+        }
+        
+    }
 
 }
