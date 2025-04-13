38d1094a2217e49e0ec016710bf99434d79b19ca
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:17:12 -0500

    v3

--- ejercicio1/Main.java
+++ ejercicio1/Main.java
@@ -2,7 +2,13 @@ package ejercicio1;
 
 public class Main {
     public static void main(String[] args) {
-        
+        int[][] entradas = {
+            {5,4,8,10,3,5,27},
+            {5,4,8,10,3,6,27},
+            {6,2,16,5,7,10,33},
+            {6,2,16,5,3,10,33},
+            {4,2,5,1,6,13}
+        };
     }
 
 }
