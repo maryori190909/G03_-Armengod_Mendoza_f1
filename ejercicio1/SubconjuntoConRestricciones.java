0cfe58124c08cb058fdb911a46027801b834e251
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:06:12 -0500

    v3

--- ejercicio1/SubconjuntoConRestricciones.java
+++ ejercicio1/SubconjuntoConRestricciones.java
@@ -1,8 +1,15 @@
 package ejercicio1;
 
+import java.util.Arrays;
+
 public class SubconjuntoConRestricciones {
     public static boolean esPotenciaDe2(int num) {
         return num > 0 && (num & (num - 1)) == 0;
     }
+    public static boolean puedeSumar(int[] arr, int pos, int objetivo) {
+        if (pos == arr.length) {
+            return objetivo == 0;
+        }
 
+}
 }
