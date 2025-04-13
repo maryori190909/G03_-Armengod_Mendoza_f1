b12b04352644c23a39fd1942da8c398179c33ad0
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:53:45 -0500

    v3

--- activida5/dpSolution.java
+++ activida5/dpSolution.java
@@ -3,6 +3,12 @@ package activida5;
 public class dpSolution {
     static int getValue(int[] values, int rodLength) {
         int[] subSolutions = new int[rodLength + 1];
-        
+        for (int i = 1; i <= rodLength; i++) {
+            int tmpMax = -1;
+            for (int j = 0; j < i; j++)
+                tmpMax = Math.max(tmpMax, values[j] + subSolutions[i - j - 1]);
+            subSolutions[i] = tmpMax;
+        }
+        return subSolutions[rodLength];
     }
 }
