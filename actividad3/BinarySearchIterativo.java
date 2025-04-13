ff64675b04aabe17a047f43609364742bd091385
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:20:08 -0500

    v2

--- actividad3/BinarySearchIterativo.java
+++ actividad3/BinarySearchIterativo.java
@@ -1,5 +1,11 @@
 package actividad3;
 
 public class BinarySearchIterativo {
-
+    int binarySearch(int arr[], int x){
+        int lo = 0, hi = arr.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+        }
+    }
 }
