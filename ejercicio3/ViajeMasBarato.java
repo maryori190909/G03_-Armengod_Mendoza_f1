dea8eb129a2fb2daa7759827c16fc087002ee9ec
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:44:40 -0500

    v5

--- ejercicio3/ViajeMasBarato.java
+++ ejercicio3/ViajeMasBarato.java
@@ -6,10 +6,16 @@ public class ViajeMasBarato {
         int[][] C = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                
-    
+                if (i == j) {
+                    C[i][j] = 0;
+                } else if (i < j) {
+                    C[i][j] = T[i][j];
+                } else {
+                    C[i][j] = Integer.MAX_VALUE;
+                }
             }
         }
+        
     }
     
 
