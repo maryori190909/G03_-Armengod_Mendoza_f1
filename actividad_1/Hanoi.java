86ef62608c9276f605627333747844705cafc365
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 20:32:24 -0500

    v3

--- actividad_1/Hanoi.java
+++ actividad_1/Hanoi.java
@@ -4,4 +4,5 @@ public class Hanoi {
     public static void main(String[] args){
         torresHanoi(3,1,2,3);
     }
+    public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
 }
