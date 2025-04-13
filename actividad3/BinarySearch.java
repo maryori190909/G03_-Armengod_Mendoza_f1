930cbba4670084cf092ad1e8f8735b9050d36349
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:04:50 -0500

    v2

--- actividad3/BinarySearch.java
+++ actividad3/BinarySearch.java
@@ -1,5 +1,9 @@
 package actividad3;
 
 public class BinarySearch {
-
+    int binarySearch(int arr[], int lo, int hi, int x) { 
+        if (hi >= lo && lo < arr.length - 1) {
+            int mid = lo + (hi - lo) / 2;
+    }
+}
 }
