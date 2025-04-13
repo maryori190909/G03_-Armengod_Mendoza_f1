745c5627ab7627631fade46493c615ca32a8a043
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:21:26 -0500

    v4

--- ejercicio2/QuickSelect.java
+++ ejercicio2/QuickSelect.java
@@ -2,5 +2,10 @@ package ejercicio2;
 import java.util.Arrays;
 
 public class QuickSelect {
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        
+    }
 
 }
