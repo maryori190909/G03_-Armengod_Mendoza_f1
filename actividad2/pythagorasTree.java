7b9219200dc8b61a3d74adea785e1a614a6b3efd
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 20:46:37 -0500

    v3

--- actividad2/pythagorasTree.java
+++ actividad2/pythagorasTree.java
@@ -5,4 +5,9 @@ import javax.swing.*;
 public class pythagorasTree extends JPanel{
     private int profundidad;
 
+    public pythagorasTree(int profundidad) {
+        this.profundidad = profundidad;
+        setPreferredSize(new Dimension(1000, 800));
+    }
+
 }
