a7fd5e8bae2b6012599fe8a4890e32b564dbfcf6
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:23:16 -0500

    v5

--- ejercicio2/QuickSelect.java
+++ ejercicio2/QuickSelect.java
@@ -5,6 +5,12 @@ public class QuickSelect {
     public static int partition(int[] arr, int low, int high) {
         int pivot = arr[high];
         int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
         
     }
 
