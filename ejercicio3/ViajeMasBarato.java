207f6bf5f154a98ea91bfafda3db945372c20196
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:46:02 -0500

    v6

--- ejercicio3/ViajeMasBarato.java
+++ ejercicio3/ViajeMasBarato.java
@@ -15,7 +15,15 @@ public class ViajeMasBarato {
                 }
             }
         }
-        
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = i + 1; k < j; k++) {
+                    
+
+                }
+            }
+        }
     }
     
 
