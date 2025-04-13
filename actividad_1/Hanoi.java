6a6866745b011c6105b9c14afe2a3e6dfff27ad3
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 20:34:38 -0500

    v4

--- actividad_1/Hanoi.java
+++ actividad_1/Hanoi.java
@@ -5,4 +5,9 @@ public class Hanoi {
         torresHanoi(3,1,2,3);
     }
     public static void torresHanoi(int discos, int torre1, int torre2, int torre3){
+        if (discos == 1){
+            System.out.println("Mover el disco de la torre " + torre1 + " a la torre " + torre3);
+        } else{
+}
+    }
 }
