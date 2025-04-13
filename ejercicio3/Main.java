201bb160b611a41a71e1486399530bd892b1d095
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 22:59:15 -0500

    v8

--- ejercicio3/Main.java
+++ ejercicio3/Main.java
@@ -13,6 +13,8 @@ public class Main {
         System.out.println("matriz de tarifas directas :");
         ViajeMasBarato.mostrarMatriz(T);
         int[][] C = ViajeMasBarato.calcularCostosMinimos(T);
+        System.out.println("\nmatriz de Costos Minimos es :");
+        ViajeMasBarato.mostrarMatriz(C);
 
     }
 
