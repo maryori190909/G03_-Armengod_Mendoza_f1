9b410f3ad6f079864306907a7686a0685630c1c4
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:59:11 -0500

    v3

--- activida5/naiveSolution.java
+++ activida5/naiveSolution.java
@@ -4,5 +4,10 @@ public class naiveSolution {
     static int getValue(int[] values, int length){
         if (length <= 0)
             return 0;
+            int tmpMax = -1;
+        for (int i = 0; i < length; i++) {
+            tmpMax = Math.max(tmpMax, values[i] + getValue(values, length - i - 1));
+        }
+        return tmpMax;
     }
 }
