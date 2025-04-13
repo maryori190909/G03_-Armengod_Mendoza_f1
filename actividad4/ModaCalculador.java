6d28f06376062253bb01363982aea8dfa5c5a9c2
maryori190909 <maryori.armengod@estudiante.ucsm.edu.pe>
2025-04-12 21:30:12 -0500

    v2

--- actividad4/ModaCalculador.java
+++ actividad4/ModaCalculador.java
@@ -1,5 +1,10 @@
 package actividad4;
 
 public class ModaCalculador {
-
+    public static int calcularModa(int[] arreglo) {
+        if (arreglo.length == 0) {
+            throw new IllegalArgumentException("el arreglo no debe estar vacio.");
+        }
+    }
 }
+
